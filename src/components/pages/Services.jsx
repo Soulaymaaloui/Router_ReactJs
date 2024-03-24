9cd645f478c89196d7df8c95ed06774932f56a6f
sou@gmail.com <soulaymaaloui557@gmail.com>
2024-03-24 15:21:57 +0100

    first commit

---
+++ src/components/pages/Services.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export const Services = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Blanditiis at molestias dolore id animi, ipsam aut dignissimos sint deleniti debitis pariatur vitae officiis necessitatibus. Facere amet nam cupiditate rem? Soluta quia reprehenderit quo, eaque totam necessitatibus! Temporibus non dolore a explicabo, dignissimos repudiandae sit error commodi maxime adipisci soluta eveniet suscipit magni aperiam! Commodi repellat quisquam, officiis at hic aspernatur nihil distinctio laboriosam voluptates unde eos magnam ipsum provident, obcaecati ad. Dignissimos quaerat est ratione dolores in aspernatur labore sint accusantium voluptatem aliquid amet, quas magnam nisi? Error nam repellat facilis, doloremque suscipit aut porro rerum veritatis beatae,
+         laudantium quasi? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Suscipit reprehenderit voluptates, blanditiis quibusdam dicta repellat molestiae minus illum quisquam deleniti sapiente doloribus nobis deserunt provident ipsum id quidem quae temporibus maxime animi. Facilis omnis, asperiores ad animi voluptas laudantium, cumque accusamus hic minus ullam architecto impedit dolor sunt? Totam fuga, ipsum cumque natus esse laboriosam dicta eveniet quia optio aliquam voluptas, aspernatur corrupti, aperiam enim recusandae accusamus vel illum. Doloribus tenetur quasi sed placeat eligendi ex accusantium blanditiis. In numquam molestias et animi doloribus odio officia eos, similique, voluptas adipisci minus aperiam assumenda, 
+         ex nobis labore aliquam aspernatur sunt! Quos.
+        <br/>
+      </div>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Blanditiis at molestias dolore id animi, ipsam aut dignissimos sint deleniti debitis pariatur vitae officiis necessitatibus. Facere amet nam cupiditate rem? Soluta quia reprehenderit quo, eaque totam necessitatibus! Temporibus non dolore a explicabo, dignissimos repudiandae sit error commodi maxime adipisci soluta eveniet suscipit magni aperiam! Commodi repellat quisquam, officiis at hic aspernatur nihil distinctio laboriosam voluptates unde eos magnam ipsum provident, obcaecati ad. Dignissimos quaerat est ratione dolores in aspernatur labore sint accusantium voluptatem aliquid amet, quas magnam nisi? Error nam repellat facilis, doloremque suscipit aut porro rerum veritatis beatae,
+         laudantium quasi? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Suscipit reprehenderit voluptates, blanditiis quibusdam dicta repellat molestiae minus illum quisquam deleniti sapiente doloribus nobis deserunt provident ipsum id quidem quae temporibus maxime animi. Facilis omnis, asperiores ad animi voluptas laudantium, cumque accusamus hic minus ullam architecto impedit dolor sunt? Totam fuga, ipsum cumque natus esse laboriosam dicta eveniet quia optio aliquam voluptas, aspernatur corrupti, aperiam enim recusandae accusamus vel illum. Doloribus tenetur quasi sed placeat eligendi ex accusantium blanditiis. In numquam molestias et animi doloribus odio officia eos, similique, voluptas adipisci minus aperiam assumenda, 
+         ex nobis labore aliquam aspernatur sunt! Quos.
+        <br/>
+      </div>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Blanditiis at molestias dolore id animi, ipsam aut dignissimos sint deleniti debitis pariatur vitae officiis necessitatibus. Facere amet nam cupiditate rem? Soluta quia reprehenderit quo, eaque totam necessitatibus! Temporibus non dolore a explicabo, dignissimos repudiandae sit error commodi maxime adipisci soluta eveniet suscipit magni aperiam! Commodi repellat quisquam, officiis at hic aspernatur nihil distinctio laboriosam voluptates unde eos magnam ipsum provident, obcaecati ad. Dignissimos quaerat est ratione dolores in aspernatur labore sint accusantium voluptatem aliquid amet, quas magnam nisi? Error nam repellat facilis, doloremque suscipit aut porro rerum veritatis beatae,
+         laudantium quasi? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Suscipit reprehenderit voluptates, blanditiis quibusdam dicta repellat molestiae minus illum quisquam deleniti sapiente doloribus nobis deserunt provident ipsum id quidem quae temporibus maxime animi. Facilis omnis, asperiores ad animi voluptas laudantium, cumque accusamus hic minus ullam architecto impedit dolor sunt? Totam fuga, ipsum cumque natus esse laboriosam dicta eveniet quia optio aliquam voluptas, aspernatur corrupti, aperiam enim recusandae accusamus vel illum. Doloribus tenetur quasi sed placeat eligendi ex accusantium blanditiis. In numquam molestias et animi doloribus odio officia eos, similique, voluptas adipisci minus aperiam assumenda, 
+         ex nobis labore aliquam aspernatur sunt! Quos.
+        <br/>
+      </div>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Blanditiis at molestias dolore id animi, ipsam aut dignissimos sint deleniti debitis pariatur vitae officiis necessitatibus. Facere amet nam cupiditate rem? Soluta quia reprehenderit quo, eaque totam necessitatibus! Temporibus non dolore a explicabo, dignissimos repudiandae sit error commodi maxime adipisci soluta eveniet suscipit magni aperiam! Commodi repellat quisquam, officiis at hic aspernatur nihil distinctio laboriosam voluptates unde eos magnam ipsum provident, obcaecati ad. Dignissimos quaerat est ratione dolores in aspernatur labore sint accusantium voluptatem aliquid amet, quas magnam nisi? Error nam repellat facilis, doloremque suscipit aut porro rerum veritatis beatae,
+         laudantium quasi? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Suscipit reprehenderit voluptates, blanditiis quibusdam dicta repellat molestiae minus illum quisquam deleniti sapiente doloribus nobis deserunt provident ipsum id quidem quae temporibus maxime animi. Facilis omnis, asperiores ad animi voluptas laudantium, cumque accusamus hic minus ullam architecto impedit dolor sunt? Totam fuga, ipsum cumque natus esse laboriosam dicta eveniet quia optio aliquam voluptas, aspernatur corrupti, aperiam enim recusandae accusamus vel illum. Doloribus tenetur quasi sed placeat eligendi ex accusantium blanditiis. In numquam molestias et animi doloribus odio officia eos, similique, voluptas adipisci minus aperiam assumenda, 
+         ex nobis labore aliquam aspernatur sunt! Quos.
+        <br/>
+      </div>
+    </div>
+  )
+}
+
+
